af39f43f8ae6ee58d1a3d5b8dc6e1f0d081dd361
David Martin <dmartin@ippon.fr>
2018-03-22 12:58:50 +0100

    Added hascode/equals to build Set

--- src/main/java/org/spuristo/core/model/SpuristoPath.java
+++ src/main/java/org/spuristo/core/model/SpuristoPath.java
@@ -52,9 +52,33 @@ public class SpuristoPath {
 		public String target;
 		public String type;
 		public List<Attribute> attributes;
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			PathRelation other = (PathRelation) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
 
 	}
 
+	@JsonAutoDetect
 	public static final class PathVertex {
 		@JsonAlias({"clazz", "_clazz"})
 		public String clazz;
@@ -62,6 +86,39 @@ public class SpuristoPath {
 		public String id;
 		@JsonAlias({"key", "_key"})
 		public String key;
-		public List<Attribute> attributes;		
+		public List<Attribute> attributes;
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			PathVertex other = (PathVertex) obj;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
+		
 	}
+
+	@JsonAutoDetect
+	public static final class VertexEdgePair {
+		@JsonAlias({"vertex", "v"})
+		public PathVertex vertex;
+		@JsonAlias({"relation", "e"})
+		public PathRelation relation;
+	}
+
 }
