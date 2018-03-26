941fb963beaa6aaed28bb4efc65e0922c54f9600
David Martin <dmartin.pro@gmail.com>
2018-03-26 21:56:18 +0200

    banner text

--- src/main/java/org/spuristo/core/SpuristoApplication.java
+++ src/main/java/org/spuristo/core/SpuristoApplication.java
@@ -1,25 +1,7 @@
-/*
- * DISCLAIMER
- *
- * Copyright 2017 ArangoDB GmbH, Cologne, Germany
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Copyright holder is ArangoDB GmbH, Cologne, Germany
- */
-
 package org.spuristo.core;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -31,10 +13,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpuristoApplication {
 
+	private static final Log log = LogFactory.getLog(SpuristoApplication.class);
+
 	public static void main(final String... args) {
+		log.info("\n\nSpuristo starting...\n"
+				+ "Optional parameters are:\n"
+				+ " --init-data        Load some sample data\n"
+				+ " --drop-database    Drop all collections in the database\n");
 
 		SpringApplication.run(SpuristoApplication.class, args);
-
 	}
 
 }
